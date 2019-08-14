a07d29ebf6dfc7dbe7f7d4026a9156bde3af1205
andrewjabraham <54107371+andrewjabraham@users.noreply.github.com>
2019-08-14 10:17:49 -0700

    Add files via upload

---
+++ FIGURE_R_SCRIPTS/SUPPLEMENTARY_FIGURES/AutotrophicBiomass_maps.R
@@ -0,0 +1,83 @@
+#Load required packages
+library(ggplot2)
+library(rgdal)
+library(maps)
+library(RNetCDF)
+
+#Load ensemble means
+a<-read.csv("/Users/andrewabraham/Dropbox/Madingley & Megabiota/Manuscript_Scripts/Autotroph_MEAN.csv", stringsAsFactors=F)
+
+#Load world boarders
+wmap<-readOGR(dsn="/Users/andrewabraham/Dropbox/Madingley & Megabiota/ne_110m_land", layer="ne_110m_land")
+wmap<-fortify(wmap)
+
+tiff('Pleistocene_AB_map.tiff', units="in",width=5, height=5, res=500)
+
+#Plot Plesitocene World
+ggplot() + coord_equal() + 
+  scale_x_continuous(name="", breaks=c(-180,0,180),labels=c("-180","0","180"),limits=c(-180,180)) + 
+  scale_y_continuous(name="", breaks=c(-90,0,90),limits=c(-90,90)) + 
+  theme_bw() + theme(axis.title.x=element_blank(),axis.title.y=element_blank()) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(panel.border = element_rect(size=1)) +
+  theme(legend.position="bottom",legend.direction="horizontal",legend.key.width=unit(1,"cm"),legend.key.height=unit(0.35,"cm")) +
+  geom_tile(data=a, aes(x=a[,2], y=a[,1], fill=a[,3]), size=1) +
+  geom_map(data=wmap,map=wmap,aes(map_id=id),fill=NA,colour="black",size=0.3) +
+  scale_fill_distiller(palette ="Spectral",na.value=NA,
+                       breaks=c(10,1500000,3000000),
+                       limits=c(0,3000000),
+                       labels=c("0e+00","1.5e+06","3e+06"),
+                       guide=guide_colourbar(title="Autotroph Biomass \n(kg/km2)"))
+
+
+dev.off()
+
+#Calculate difference
+d<-data.frame(a[,4]-a[,3])
+e<-data.frame(a[,5]-a[,3])
+
+d<-data.frame(a[,1:2],d[,1])
+e<-data.frame(a[,1:2],e[,1])
+
+tiff('Sim1000_AB_map.tiff', units="in",width=5, height=5, res=500)
+
+#Plot Sim_1000_ensemble difference
+ggplot() + coord_equal() +  
+  scale_x_continuous(name="", breaks=c(-180,0,180),labels=c("-180","0","180"),limits=c(-180,180)) + 
+  scale_y_continuous(name="", breaks=c(-90,0,90),limits=c(-90,90)) + 
+  theme_bw() + theme(axis.title.x=element_blank(),axis.title.y=element_blank()) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(panel.border = element_rect(size=1)) +
+  theme(legend.position="bottom",legend.direction="horizontal",legend.key.width=unit(1,"cm"),legend.key.height=unit(0.35,"cm")) +
+  geom_tile(data=d, aes(x=d[,2], y=d[,1], fill=d[,3]), size=1) +
+  geom_map(data=wmap,map=wmap,aes(map_id=id),fill=NA,colour="black",size=0.3) +
+  scale_fill_distiller(palette ="PuOr",na.value=NA,
+                       breaks=c(-1700000,0,1700000),
+                       limits=c(-1700000,1700000),
+                       labels=c("-1.7e+06","0","1.7e+06"),
+                       guide=guide_colourbar(title="Difference (kg/km2)\n "))
+
+dev.off()
+
+
+tiff('Sim100_AB_map.tiff', units="in",width=5, height=5, res=500)
+
+#Plot Sim_100_ensemble difference
+ggplot() + coord_equal() +  
+  scale_x_continuous(name="", breaks=c(-180,0,180),labels=c("-180","0","180"),limits=c(-180,180)) + 
+  scale_y_continuous(name="", breaks=c(-90,0,90),limits=c(-90,90)) + 
+  theme_bw() + theme(axis.title.x=element_blank(),axis.title.y=element_blank()) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(panel.border = element_rect(size=1)) +
+  theme(legend.position="bottom",legend.direction="horizontal",legend.key.width=unit(1,"cm"),legend.key.height=unit(0.35,"cm")) +
+  geom_tile(data=e, aes(x=e[,2], y=e[,1], fill=e[,3]), size=1) +
+  geom_map(data=wmap,map=wmap,aes(map_id=id),fill=NA,colour="black",size=0.3) +
+  scale_fill_distiller(palette ="PuOr",na.value=NA,
+                       breaks=c(-1700000,0,1700000),
+                       limits=c(-1700000,1700000),
+                       labels=c("-1.7e+06","0","1.7e+06"),
+                       guide=guide_colourbar(title="Difference (kg/km2)\n "))
+
+dev.off()
+
+
